a4c7a8ac60d68bf7f8ace2a1cd3718065285d84e
KarynaPosokhina <karina@posokhina.com>
2024-07-24 18:10:25 +0300

    Add handleIncrement, handleDecrement

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -21,16 +21,24 @@ const CartItem = ({ onContinueShopping }) => {
    onContinueShopping(e);
   };
 
-
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+  };
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({name: item.name, quantity: item.quantity + 1}));
   };
 
   const handleDecrement = (item) => {
-   dispatch(updateQuantity());
+    if (item.quantity > 1) {
+      dispatch(updateQuantity({name: item.name, quantity: item.quantity - 1}));
+    } else {
+      dispatch(removeItem(item.name));
+    }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
